a7eb3c15541409b95f00a5721d19c51eba9493c6
Volcano <franek.kg@gmail.com>
2021-02-23 15:50:47 +0100

    Update config.py

--- Source/config.py
+++ Source/config.py
@@ -3,4 +3,4 @@ import os
 MUMBLE_BOT_TOKEN = os.environ["MUMBLE_BOT_TOKEN"]
 DISCORD_BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
 
-MONGO_URL = os.environ["MONGO_URL]
+MONGO_URL = os.environ["MONGO_URL"]
